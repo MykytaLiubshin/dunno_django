efe44700de0d7a457320074b1a2636b62e2581a9
MykytaLiubshin <enotich13@gmail.com>
2020-11-14 01:52:12 +0200

    Refactoring. Posts folder complete, I guess.

--- Dunno/posts/comments_urls.py
+++ Dunno/posts/comments_urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from posts.api.views.comment_views import ListComments
 
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path("", ListComments.as_view()),
     path("id_<str:pk>", ListComments.as_view()),
--- Dunno/posts/task.py
+++ Dunno/posts/task.py
@@ -1,16 +1,19 @@
 from posts.models import Post
 from time import sleep
 from threading import Thread
+from typing import Callable
+#Constants
+FLUSH_INTERVAL: int = 60 * 60 * 24
 
-FLUSH_INTERVAL = 60 * 60 * 24
-
-
-def bg(fn, interval=FLUSH_INTERVAL, *args, **kwargs):
+# bg - a function to wrap a task function into an execution thread
+def bg(fn: Callable, interval:int = FLUSH_INTERVAL, *args, **kwargs):
     r = Thread(target=fn, args=([interval]), daemon=True)
     r.start()
 
-
-def task(interval):
+# task - a function to do one certain task
+# to reset (flush) all the upvotes once a day
+# might rewrite later
+def task(interval: int) -> None:
     while True:
         Post.objects.all().update(upvotes=0)
         print("All upvotes are flushed.")
